ad836fbd1f3efad7855981c1629f7f5e284ec956
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-28 16:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function initForm() {
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
-  document.getElementById("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
 function createRecipe(){
@@ -13,7 +13,9 @@ function createRecipe(){
 
 function updateRecipe() {
   var recipe = getRecipeVals()
-  var recipeTemplate = document.getElementById()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
 }
 
 
