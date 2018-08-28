fdacf4c1b424e1e92d98dc44f125e7c4f0b51d10
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-28 16:31:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function initForm() {
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementById("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+
+
+
+
+
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
